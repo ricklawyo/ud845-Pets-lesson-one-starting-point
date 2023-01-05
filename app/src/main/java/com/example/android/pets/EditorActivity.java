606500dd47bad52571af7573c373b2c4f6c7fd3e
Rick <rick@laplantes.net>
2023-01-05 13:08:00 -0600

    Initial commit

---
+++ app/src/main/java/com/example/android/pets/EditorActivity.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.pets;
+
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import androidx.core.app.NavUtils;
+import androidx.appcompat.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+
+/**
+ * Allows user to create a new pet or edit an existing one.
+ */
+public class EditorActivity extends AppCompatActivity {
+
+    /** EditText field to enter the pet's name */
+    private EditText mNameEditText;
+
+    /** EditText field to enter the pet's breed */
+    private EditText mBreedEditText;
+
+    /** EditText field to enter the pet's weight */
+    private EditText mWeightEditText;
+
+    /** EditText field to enter the pet's gender */
+    private Spinner mGenderSpinner;
+
+    /** int that holds the gender of the animal */
+    private int mGender = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+
+        // Find all relevant views that we will need to read user input from
+        mNameEditText = (EditText) findViewById(R.id.edit_pet_name);
+        mBreedEditText = (EditText) findViewById(R.id.edit_pet_breed);
+        mWeightEditText = (EditText) findViewById(R.id.edit_pet_weight);
+        mGenderSpinner = (Spinner) findViewById(R.id.spinner_gender);
+
+        setupSpinner();
+    }
+
+    /**
+     * Setup the dropdown spinner that allows the user to select the gender of the pet.
+     */
+    private void setupSpinner() {
+        // Create adapter for spinner. The list options are from the String array it will use
+        // the spinner will use the default layout
+        ArrayAdapter genderSpinnerAdapter = ArrayAdapter.createFromResource(this,
+                R.array.array_gender_options, android.R.layout.simple_spinner_item);
+
+        // Specify dropdown layout style - simple list view with 1 item per line
+        genderSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+
+        // Apply the adapter to the spinner
+        mGenderSpinner.setAdapter(genderSpinnerAdapter);
+
+        mGenderSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            // Set up code that will run when the spinner option is changed
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // store the text of the selected item
+                String selection = (String) parent.getItemAtPosition(position);
+
+                // if the text isn't empty (ie has a value)
+                if(!TextUtils.isEmpty(selection)){
+                    // figure out the string value and then set mGender correctly
+                    if(selection.equals(("Male")))
+                        mGender = 1; // male
+                    else if(selection.equals("Female"))
+                        mGender = 2; // female
+                    else
+                        mGender = 0; // unknown
+                }
+            }
+
+            // Because AdapterView is an abstract class, onNothingSelected must be defined
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // default value if nothing selected will be unknown...
+                mGender = 0;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_editor.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+}
